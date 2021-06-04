7b3f55cc48a339b32a30c3ac9bc37740e74f79f5
sanjanamn <sanjana@uxltechnologies.net>
2021-06-04 15:00:23 +0530

    made changes to the stored procedure

---
+++ Programmability/Procedures/dbo.GetProductDesc_withparameters.sql
@@ -0,0 +1,15 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[GetProductDesc_withparameters]
+(@PID INT)
+AS
+BEGIN
+SET NOCOUNT ON
+ 
+SELECT P.ProductID,P.ProductName,PD.ProductDescription  FROM 
+Product P
+INNER JOIN ProductDescription PD ON P.ProductID=PD.ProductID
+WHERE P.ProductID=@PID
+ 
+END
+GO
