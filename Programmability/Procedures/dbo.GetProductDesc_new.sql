603cf3bdf03a183b7df77a9ae75d211dc6415227
sanjanamn <sanjana@uxltechnologies.net>
2021-06-07 19:08:18 +0530

    Commiting my changes on stored procedure to test

--- Programmability/Procedures/dbo.GetProductDesc_new.sql
+++ Programmability/Procedures/dbo.GetProductDesc_new.sql
@@ -5,9 +5,8 @@ AS
 BEGIN
 SET NOCOUNT ON
  
-SELECT P.ProductID,P.ProductName,PD.ProductDescription  FROM 
-Product P
-INNER JOIN ProductDescription PD ON P.ProductID=PD.ProductID
+SELECT ProductID,ProductName  FROM 
+Product 
 
 
  
