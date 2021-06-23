e71c506ce676972aebc223d5d297d5f0d7108a3e
sanjanamn <sanjana@uxltechnologies.net>
2021-06-23 20:26:15 +0530

    Comminting local changes

--- Programmability/Procedures/dbo.GetProductDesc_new.sql
+++ Programmability/Procedures/dbo.GetProductDesc_new.sql
@@ -23,3 +23,23 @@ END
 GO
 
 
+
+SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+
+
+
+
+
+CREATE PROCEDURE [dbo].[GetProductDesc_new]
+AS
+BEGIN
+SET NOCOUNT ON
+ 
+SELECT ProductID  FROM 
+Product 
+
+
+ 
+END
+GO
