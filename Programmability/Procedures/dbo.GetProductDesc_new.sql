98c61a92940b30b07ada53afe3b29e5f69dbc420
Thanvi Ponamma <51111819+ThanviPonamma@users.noreply.github.com>
2021-06-23 16:25:21 +0530

    Update dbo.GetProductDesc_new.sql

--- Programmability/Procedures/dbo.GetProductDesc_new.sql
+++ Programmability/Procedures/dbo.GetProductDesc_new.sql
@@ -1,14 +1,25 @@
-﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+USE [SampleDb]
 GO
-CREATE PROCEDURE [dbo].[GetProductDesc_new]
+
+/****** Object:  StoredProcedure [dbo].[GetProductDesc_new]    Script Date: 23-06-2021 11:42:07 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[GetProductDesc_new]
 AS
 BEGIN
 SET NOCOUNT ON
  
-SELECT ProductID,ProductName  FROM 
-Product 
+SELECT P.ProductID,P.ProductName,PD.ProductDescription  FROM 
+Product P
+INNER JOIN ProductDescription PD ON P.ProductID=PD.ProductID
 
 
  
 END
 GO
+
+
